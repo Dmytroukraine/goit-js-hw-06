0434fcc5079b9a79d0a7b0bbd1d3105e49687adb
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-05-01 19:52:52 +0300

    Task 1 - repaire

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,9 @@
-// const categoriesList = document.querySelector('#categories');
-// const categoriesItems = categoriesList.querySelectorAll('.item');
-// console.log(`Number of categories: ${categoriesItems.length}\n`);
-// categoriesItems.forEach(item => {
-//   const categoryName = item.querySelector('h2').innerText;
-//   const categoryElementsCount = item.querySelector('ul').childElementCount;
-//   console.log(`Category: ${categoryName}`);
-//   console.log(`Elements: ${categoryElementsCount}\n`);
-// });
-
-
-
+const categoriesList = document.body.querySelector("#categories");
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+console.log(`Number of categories: ${categoriesItems.length}`);
+categoriesItems.forEach((item) => {
+  const categoryName = item.querySelector("h2").textContent;
+  const categoryElements = item.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements}`);
+});
