9eb91d60540f1ed29fe8927471a91b5ac9a8e932
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-05-01 19:42:35 +0300

    Task 7 repaire

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
-const categoriesList = document.querySelector('#categories');
-const categoriesItems = categoriesList.querySelectorAll('.item');
+// const categoriesList = document.querySelector('#categories');
+// const categoriesItems = categoriesList.querySelectorAll('.item');
+// console.log(`Number of categories: ${categoriesItems.length}\n`);
+// categoriesItems.forEach(item => {
+//   const categoryName = item.querySelector('h2').innerText;
+//   const categoryElementsCount = item.querySelector('ul').childElementCount;
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${categoryElementsCount}\n`);
+// });
 
-console.log(`Number of categories: ${categoriesItems.length}\n`);
 
-categoriesItems.forEach(item => {
-  const categoryName = item.querySelector('h2').innerText;
-  const categoryElementsCount = item.querySelector('ul').childElementCount;
 
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElementsCount}\n`);
-});
